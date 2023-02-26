56ffb6217a6906ac5b3e7f9be0665530cba37b34
demahomAit <125216413+demahomAit@users.noreply.github.com>
2023-02-26 18:44:13 +0100

    Update DesDecrypter.java

--- src/desprogram/DesDecrypter.java
+++ src/desprogram/DesDecrypter.java
@@ -1,5 +1,22 @@
 package desprogram;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.util.*;
+import javax.crypto.Cipher;
+import javax.crypto.KeyGenerator;
+import javax.crypto.SecretKey;
 
 public class DesDecrypter {
+    Cipher dcipher;
+
+    public DesDecrypter(SecretKey key) throws Exception {
+        dcipher = Cipher.getInstance("DES");
+        dcipher.init(Cipher.DECRYPT_MODE, key);
+    }
+
+    public byte[] decrypt(byte[] input) throws Exception {
+        return dcipher.doFinal(input);
+    }
 
 }
+
