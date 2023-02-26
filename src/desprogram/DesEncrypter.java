a6ae741d40c8f438c171de735f2100fd69f3114d
demahomAit <125216413+demahomAit@users.noreply.github.com>
2023-02-26 18:44:53 +0100

    Update DesEncrypter.java

--- src/desprogram/DesEncrypter.java
+++ src/desprogram/DesEncrypter.java
@@ -1,5 +1,14 @@
 package desprogram;
-
+import javax.crypto.Cipher;
+import javax.crypto.SecretKey;
 public class DesEncrypter {
+    Cipher ecipher;
+    public DesEncrypter(SecretKey key) throws Exception {
+        ecipher = Cipher.getInstance("DES");
+        ecipher.init(Cipher.ENCRYPT_MODE, key);
+    }
+	public byte[] encrypt(byte[] input) throws Exception {
+        return ecipher.doFinal(input);
+    }
 
 }
